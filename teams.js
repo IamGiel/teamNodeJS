2fcbb4f459ec78467c1f2d6964ea27bc8d3c3fdd
Server <tilesmaster@tiless-mbp.attlocal.net>
2017-12-06 00:05:45 -0500

    edit spaces

--- teams.js
+++ teams.js
@@ -35,13 +35,10 @@ var Player = function(Name, Position, Offense, Defense) {
       }
     }
 };   
-  
-
-
-  
+  //here inquire.prompt
   var count = 0;
   var profiler = function() {
-      if (count < 4) {
+      if (count < 4) {//number of players
         inquirer.prompt([
             {
              name: "Name",
@@ -64,6 +61,7 @@ var Player = function(Name, Position, Offense, Defense) {
           ]).then(function(answer) {
             var newPlayer = new Player(answer.Name, answer.Position, answer.Offense, answer.Defense);
             console.log(answer.Name);
+            //work-around - list players numerically
             var list = count;
             var playerList = list + 1;
             var loggedInfo =  "\n================== Player: " + playerList + " ====================" + 
@@ -71,12 +69,13 @@ var Player = function(Name, Position, Offense, Defense) {
               "Position: " + answer.Position + "\n" +
               "Offense pts: " + answer.Offense + "\n" +
               "Defense pts: " + answer.Defense + "\n";
+              //here store info in log.txt
             fs.appendFile("log.txt", loggedInfo, function (err) {
               if (err) throw err;
               console.log('Saved!');
               profiler();
             }); 
-              count++;
+              count++;//keep asking until max players reached
               
           });
     }
@@ -86,45 +85,4 @@ var Player = function(Name, Position, Offense, Defense) {
   } 
   profiler();
 
-  // //==========================================
-
-  // // dependency for inquirer npm package
-  // var inquirer = require("inquirer");
-
-  // // constructor function used to create programmers objects
-  // function Programmer(name, position, age, language) {
-  //   this.name = name;
-  //   this.position = position;
-  //   this.age = age;
-  //   this.language = language;
-  //   // creates the printInfo method and applies it to all programmer objects
-  //   this.printInfo = function() {
-  //     console.log("Name: " + this.name + "\nPosition: " + this.position +
-  //     "\nAge: " + this.age + "\nLanguages: " + this.language);
-  //   };
-  // }
-
-  // // runs inquirer and asks the user a series of questions whose replies are
-  // // stored within the variable answers inside of the .then statement
-  // inquirer.prompt([
-  //   {
-  //     name: "name",
-  //     message: "What is your name?"
-  //   }, {
-  //     name: "position",
-  //     message: "What is your current position?"
-  //   }, {
-  //     name: "age",
-  //     message: "How old are you?"
-  //   }, {
-  //     name: "language",
-  //     message: "What is your favorite programming language?"
-  //   }
-  // ]).then(function(answers) {
-  //   // initializes the variable newGuy to be a programmer object which will take
-  //   // in all of the user's answers to the questions above
-  //   var newGuy = new Programmer(answers.name, answers.position, answers.age, answers.language);
-  //   // printInfo method is run to show that the newguy object was successfully created and filled
-  //   newGuy.printInfo();
-  // });
-
+ 
